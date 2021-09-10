aed2e76746d67e572df50b855622e7957e3e9b08
Jeong Pak <dannypak716@gmail.com>
2021-09-10 18:23:23 -0400

    styled details dropdown and finished

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
+const StyledDetails = styled.div`
+    text-align: center;
+    background-color: darkorange;
+    margin: 1% 30%;
+    border-radius: 70px;
+    color: white;
+    opacity: .9;
+    h2{
+        color: black;
+    }
+    h3{
+        color: black;
+    }
+    button{
+        background-color: grey;
+        color: white;
+        margin-bottom: 2%;
+        &:hover{
+            background-color: black;
+            color: white;
+        }
+    }
+`
 
 
 
@@ -18,13 +42,13 @@ export default function Character(props){
 
 
     return(
-        <div>
+        <StyledDetails>
             <h2>Details of {details.name}</h2>
             <h3>{details.name} was born in {details['birth_year']}</h3>
             <h4>Height: {details.height} <br></br> Weight: {details.mass}</h4>
             <p>Hair Color: {details['hair_color']} Skin Color: {details['skin_color']} Eye Color: {details['eye_color']}
             </p>
             <button onClick={close}>Close</button>
-        </div>
+        </StyledDetails>
     )
 }
